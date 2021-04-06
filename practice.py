2c7f625ff829f54774dac0dfdb519efa06f9aee4
veroca88 <veronicadonoso1506@gmail.com>
2021-04-05 20:56:08 -0500

    Python Object and data structure basics

---
+++ practice.py
@@ -0,0 +1,36 @@
+# To print anything use print
+print('hello world')
+
+# Python uses Dynamic Typing (JS, C++ is statically-typed)
+my_brothers = 2
+type(my_brothers)
+output int
+
+# Assign to the same variable a different value
+my_brothers = ["Paul", "Jonh"]
+output list
+
+# To check the length of the string
+len('I love hiking')
+output 13
+
+# indexing
+myString = "I love the mountains"
+myString[0]
+output I
+
+# Reverse indexing
+myString[-2]
+output n
+
+# Slice string starting at 2 index (up to but not included)
+myString[2:]
+output love the mountains
+
+# Slice string starting from 0 to 4 index (up to but not included)
+myString[:4]
+output I lo
+
+# Slice string in the middle
+myString[2:6]
+output love
